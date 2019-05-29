e9688765077d3dfd2ad8b1c013896c82397277b5
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-05-29 10:32:06 +0200

    vorerst dirty fix für langswap

--- web3/src/main/java/application/sichtung/SichtungsController.java
+++ web3/src/main/java/application/sichtung/SichtungsController.java
@@ -8,23 +8,49 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 @Controller
-@SessionAttributes(names = {"sichtungen", "langobject"})
+@SessionAttributes(names = {"sichtungen", "langObject", "currentLang"})
 public class SichtungsController {
     private Logger logger = LoggerFactory.getLogger(SichtungsController.class);
+
+    public class langObject {
+        private String[] langs = {"de", "en"};
+        private String currLang = "de";
+
+        public String getCurrLang() {
+            return currLang;
+        }
+
+        public String[] getLangs() {
+            return langs;
+        }
+
+        public void setCurrLang(String currLang) {
+            this.currLang = currLang;
+        }
+
+        public void setLangs(String[] langs) {
+            this.langs = langs;
+        }
+    }
 
     @ModelAttribute("sichtungen")
     public void init_model(Model m) {
         m.addAttribute("sichtungen", new Sichtungen());
     }
 
+    @ModelAttribute("langObject")
+    public void init_langs(Model m) {
+        m.addAttribute("langObject", new langObject());
+    }
 
     @GetMapping("/sichtung")
     public String showSichtungen(@ModelAttribute("sichtungsform") Sichtung sichtungsform, Model m) {
         logger.info("Sichtungsform: " + sichtungsform);
         m.addAttribute("sichtungsform", sichtungsform);
-        m.addAttribute("langobject", "de");
+        m.addAttribute("currentLang", "de");
         return "sichtungen";
     }
 
@@ -50,8 +76,9 @@ public class SichtungsController {
     }
 
     @PostMapping(value = "/sichtung", params = "sprache")
-    public String swapLang(@ModelAttribute("sichtungsform") Sichtung sichtungsform, Model m, @ModelAttribute("langobject") String langobject) {
+    public String swapLang(@ModelAttribute("sichtungsform") Sichtung sichtungsform, Model m, @ModelAttribute("langObject") langObject langobject) {
         m.addAttribute("sichtungsform", sichtungsform);
+        m.addAttribute("currLang", langobject.getCurrLang());
         return "sichtungen";
     }
 }
