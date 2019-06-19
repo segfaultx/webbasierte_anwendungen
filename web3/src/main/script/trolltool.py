1e6a0299f6557fd485bf175087990cea41da2b80
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-06-19 11:34:49 +0200

    fix trolltool

--- web3/src/main/script/trolltool.py
+++ web3/src/main/script/trolltool.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
-import sys, requests
+import sys, requests, re, random
+
+ptrn = re.compile(r"ama")
 
 trollusers = [("troll1", "troll"), ("troll2", "troll"), ("troll3", "troll")]
 
-comments_to_troll = [] # remember comment ids to troll for later
+comments_to_troll = []  # remember comment ids to troll for later
 # get all sightings + comments
 
 r = requests.get("http://localhost:8080/rest/sichtungen")
 
 for item in r.json():
-    req = requests.get("http://localhost:8080/"+item)
+    req = requests.get("http://localhost:8080" + item)
     for comment in req.json()["commentList"]:
-        print(comment["fullname"])
+        if ptrn.match(comment["fullname"]):
+            comments_to_troll.append((item.split("/")[-1], comment["commentId"]))
+for sichtung, comment in comments_to_troll:
+    trolluser = random.choice(trollusers)[0]
+    requests.post(f"http://localhost:8080/rest/sichtungen/{sichtung}/kommentare",
+                  data={'sichtungId': 0, 'message': "FAKE NEWS!!!!",
+                        'creator': {'loginname': trolluser}, "fullname": trolluser})
